14e7c191afa22a6b7a886661d8fb4d39ff25a49e
Ade <https://github.com/AdeShennaike>
2023-12-04 13:37:50 -0500

    created some variables and functions and also got the card decks to properly fill and unfill

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 //Deck of 52 cards. The face cards are hard coded while a for loop fills in the other cards.
-const deck = ["sk", "ck", "hk", "dk", "sq", "cq", "hq", "dq", "sj", "cj", "hj", "dj", "sa", "ca", "ha", "da"]
+const deck = ["sK", "cK", "hK", "dK", "sQ", "cQ", "hQ", "dQ", "sJ", "cJ", "hJ", "dJ", "sA", "cA", "hA", "dA"]
+
+//Variables
+let wager
+let moneyEarned
+let handSum
+let winner
+let playerHand
+let dealerHand
+const results = document.querySelector(".results")    
+const cardSuit = document.querySelector(".suit")
+const cardRank = document.getElementsByClassName("rank")
 
 //Function fills in the rest of the card array
-function cardFill(){
+function deckFill(){
     for(let i = 2; i < 11; i++){
         deck.push(`s${i}`)
     }
@@ -17,8 +28,48 @@ function cardFill(){
     }
 }
 
-cardFill()
-console.log(deck)
+// cardSuit.innerHTML = "&#9830;&#65039;"
+// for(let i = 0; i < cardRank.length; i++){
+    //     if(cardRank[i].innerHTML === "A")
+    // }
+    
 
-const cardSuit = document.querySelector(".suit")
-// cardSuit.innerText = "&#9830;&#65039;"
+//draws random card from deck to players hand.
+function dealPlayerCards(){
+    for(let i = 0; i < 2; i++){
+        playerHand.push(deck[Math.floor(Math.random() * 53)])
+    }
+}
+
+//draws random card from deck to players hand.
+function dealDealerCards(){
+    for(let i = 0; i < 2; i++){
+        dealerHand.push(deck[Math.floor(Math.random() * 53)])
+    }
+}
+
+//removes cards drawn from the deck that are in the dealer and players hands.
+function removeCards(){
+    for(let i = 0; i < 2; i++){
+        deck.splice(deck.indexOf(playerHand[i].toString()), 1)
+        deck.splice(deck.indexOf(dealerHand[i].toString()), 1)
+    }
+}
+
+function init(){
+    wager = 0;
+    moneyEarned = 0;
+    handSum = 0;
+    winner = null
+    deckFill()
+    playerHand = []
+    dealerHand = []
+}
+
+init()
+// dealPlayerCards()
+// dealDealerCards()
+// removeCards()
+// console.log(playerHand)
+// console.log(dealerHand)
+// console.log(deck)
